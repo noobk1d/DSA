8cd97e5aa6aec70c039995b691c37f4b938a6edd
aadarsh1202 <aadarsh.21810861@viit.ac.in>
2023-10-18 23:41:28 +0530

    Java Basic Updated

--- Codes/demo.java
+++ Codes/demo.java
@@ -1,5 +1,21 @@
 class demo {
     public static void main(String[] args) {
-        System.out.println("1"); 
+        greaterNumber(3,2,1); 
+    }
+    
+    static void greaterNumber(int a,int b,int c){
+    	if(a > b){
+    		if(a > c){
+    			System.out.println("A is Greatest.");
+    		}else{
+    			System.out.println("B is Greatest.");
+    		}
+    	}else{
+    		if(b > c){
+    			System.out.println("B is Greatest.");
+    		}else{
+    			System.out.println("C is Greatest.");
+    		}
+    	}
     }
 }
