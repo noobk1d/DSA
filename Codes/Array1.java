0b70c16e84ede548a0c9c881a087583dd8f0d618
noobk1d <practicewithmi1@gmail.com>
2023-11-10 19:09:40 +0530

    Array1 Updated

--- Codes/Array1.java
+++ Codes/Array1.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;  
 class Array1{
     public static void main(String[] args) {
     
@@ -15,6 +16,10 @@ class Array1{
         for(int i=0;i<eles;i++){
         	System.out.println(a1[i]);
         }
+        //Largest Number
+        System.out.println("Largest Number:"+largestNumber(a1));
+        //Second Largest 
+        System.out.println("Second Largest Number:"+secondLargest(a1));
         
     }
     
@@ -45,5 +50,52 @@ class Array1{
  		}
  		return res;
  }
+ 
+ 	public static int largestNumber(int []a){
+ 		int res = a[0];
+ 		int length = a.length;
+ 		for(int i=1; i<length; i++){
+ 			if(a[i] > res){
+ 				res = a[i];
+ 			}
+ 		}
+ 		return res;
+ 	}
+ 	
+ 	public static int secondLargest(int []a){
+ 		int result = -1;
+ 		int length = a.length;
+ 		//O(2n) approach
+ 		// int largestNumber = largestNumber(a);
+ 		// for(int i=0; i<length; i++){
+ 		// 	if(a[i] < largestNumber && a[i] > result){
+ 		// 		result = a[i];
+ 		// 	}
+ 		// }
+ 		// return result;
+ 		
+ 		//Approach 2 Sort array O(nlogn) Solution not appropriate Ex : 1 4 5 6 6 
+ 		// 5 is the ans as 6 repeats 2 times which is highest.Ans according to this is 6.
+ 		//Solution for unique elements.
+ 		// Arrays.sort(a);
+ 		// for(int i=0;i<length;i++){
+   //      	System.out.println(a[i]);
+   //      }
+ 		// return a[length-2];
+ 		
+ 		//O(n) Two variable
+ 		int largestNumber = a[0];
+ 		for(int i=1; i<length;i++){
+ 			if(a[i] > largestNumber){
+ 				result = largestNumber;
+ 				largestNumber = a[i];
+ 			}else if(a[i] < largestNumber){
+ 				if(result == -1 || a[i] > result){
+ 						result = a[i];
+ 				}
+ 			}
+ 		}
+ 		return  result;
+ 	}
 }
 
